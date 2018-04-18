751fc2e5e4ba96da7573caccc41b3537440cc9da
zcm <690779158@qq.com>
2018-04-18 17:19:44 +0800

    主页功能修改，2018-4-18下午

--- src/main/java/com/example/bookstore/controller/ConditionalController.java
+++ src/main/java/com/example/bookstore/controller/ConditionalController.java
@@ -23,6 +23,14 @@ public class ConditionalController {
 //        return "conditional";
 //    }
 
+    /**
+     * 条件查询
+     * @param model
+     * @param request
+     * @param session
+     * @return
+     * @throws Exception
+     */
     @RequestMapping(value = "/byCondi" , method = RequestMethod.POST)
     public String bycondi(Model model, HttpServletRequest request,HttpSession session) throws Exception {
         System.out.println("Enter byCondi");
@@ -54,5 +62,44 @@ public class ConditionalController {
         return "conditional";
     }
 
+    /**
+     * 首页条件推送链接
+     * @param model
+     * @param request
+     * @param session
+     * @return
+     * @throws Exception
+     */
+    @RequestMapping(value = "/byMessage1" , method = RequestMethod.GET)
+    public String byMess1(Model model, HttpServletRequest request,HttpSession session) throws Exception {
+        System.out.println("Enter byMess");
+        //搜索框按书名查找图书
+        String condition = "新书上架";
+        System.out.println("conditional:" + condition);
+        List<Book> books = bookService.selectNewBook()  ;
+        System.out.println("byCondi查到book：" + books);
+        //添加书本信息
+        model.addAttribute("books", books);
+        //添加查询关键字
+        model.addAttribute("keyword",condition);
+        //添加查询结果数量
+        model.addAttribute("count",books.size());
+        //判断用户登陆状态
+        if(session.getAttribute("loginid") != null ){
+            //获取session
+             String name = (String) session.getAttribute("loginname");
+            int uid = (Integer) session.getAttribute("loginid");
+            System.out.println("Conditional收到login用户name：" + name);
+            System.out.println("Conditional收到login用户id：" + uid);
+            //封装到cus对象
+            Customer cus = new Customer();
+            cus.setCustomerid(uid);
+            cus.setCustomername(name);
+            model.addAttribute("currentCus",cus);
+        }else {
+            System.out.println("当前未登录！！！！！！！！！！！！！！！！！");
+        }
+        return "conditional";
+    }
 
 }
